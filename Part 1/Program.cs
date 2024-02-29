ddd569c5d2236b7d3ec606d6ebe658a2cad1df6d
Gemma Ryan <S00250864@atu.ie>
2024-02-29 10:38:15 +0000

    Add project files.

---
+++ Part 1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            //try
+            //{
+            //    double num1 = 10;
+            //    double num2 = 0;
+            //    double result = num1 / num2;
+            //    Console.WriteLine("Result: " + result);
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+            //}
+
+
+            //Q2
+            //try
+            //{
+            //    string stringInput = "hello";
+            //    int input = int.Parse(stringInput);
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            //Q3
+            //try
+            //{
+            //    int[] smallArray = { 1, 2, 3, 4, 5 };
+
+            //    for (int i = 0; i < 9; i++)
+            //    {
+            //        int trying = smallArray[i];
+            //    }
+            //}
+            //catch (IndexOutOfRangeException ex) 
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            //Q4
+            //try
+            //{
+            //    string myString = null;
+            //    int length = myString.Length;
+            //}
+            //catch (NullReferenceException ex) 
+            //{
+            //    Console.WriteLine(ex);
+            //}
+
+
+            //Q5
+            //checked
+            //{
+            //    int num1 = int.MaxValue;
+            //    Console.WriteLine(num1);
+            //    int num2 = 10;
+            //    int result = num1 + num2;
+            //    Console.WriteLine("Result: " + result);
+            //}
+            
+
+               
+
+
+
+
+
+        }
+    }
+}
---
+++ Part 2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MethodA();
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Caught in the index out of range exception handler in the main method" + ex.Message);
+                Console.WriteLine("The stack trace is " + ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught in the general exception handler in the main method" + ex.Message);
+                Console.WriteLine("The stack trace is " + ex.StackTrace);
+            }
+        }
+        static void MethodA()
+        {
+            MethodB();
+        }
+
+        static void MethodB()
+        {
+            MethodC();
+        }
+        static void MethodC()
+        {
+            MethodD();
+        }
+        static void MethodD()
+        {
+            int[] numbers = new int[5];
+            numbers[5] = 10;
+            Console.WriteLine("Number at index 5: " + numbers[5]);
+        }
+    }
+}
+
